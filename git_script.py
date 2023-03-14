4bd45157b5dfe68aceb9eba525c7909dc05f2ee5
Fedor Metsger <fedor.metsger@gmail.com>
2023-03-14 17:18:05 +0300

    Modify git_script.py

--- git_script.py
+++ git_script.py
@@ -1,13 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import os
+import sys
 
-os.chdir(r"C:\CODE\Netology\DevOps\devops-netology")
+if len(sys.argv) > 1:
+    PATH=sys.argv[1]
+else:
+    PATH=input("Введите путь к локальному репозиторию: ")
+#PATH = r"C:\CODE\Netology\DevOps\devops-netology"
+os.chdir(PATH)
+
 bash_command = ["git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
 is_change = False
 for result in result_os.split('\n'):
     if result.find('modified') != -1:
-        prepare_result = os.pathresult.replace('\tmodified:   ', '')
-        print(prepare_result)
-        break
+        prepare_result = result.replace('\tmodified:   ', '')
+        print(os.path.join(PATH, prepare_result))
